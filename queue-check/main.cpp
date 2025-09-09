1cd53e9b27fd2c301c63d2b388d5b9036e9777ee
Nat Silprasert <natsilprasert@gmail.com>
2025-09-09 17:24:53 +0700

    queue check

--- queue-check/main.cpp
+++ queue-check/main.cpp
@@ -9,11 +9,30 @@ int main() {
     while (n--) {
         int mFront, mSize, mCap, last, correction;
         cin >> mFront >> mSize >> mCap >> last >> correction;
-        if (correction == 0) {
-
+        if (mFront < mCap &&
+            mCap >= mSize &&
+            last < mCap &&
+            (mFront + mSize) % mCap == last)
+        {
+            cout << "OK\n";
+            continue;
         }
-        else {
-            
+           
+        cout << "WRONG" << " ";
+        if (correction == 0) cout << "\n";
+
+        if (correction == 1) {
+            cout << (last + mCap - mSize) % mCap << "\n";
+        }
+        if (correction == 2) {
+            cout << (mCap + last - mFront) % mCap << "\n";
+        }
+        if (correction == 3) {
+            if (mFront < last) cout << mSize + mFront + 1 << "\n";
+            else cout << mFront - last + mSize << "\n";
+        }
+        if (correction == 4) {
+            cout << (mFront + mSize) % mCap << "\n";
         }
     } 
 
