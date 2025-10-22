28505e4cd7a9269c32826a9d1caa45cd61bf55a9
Nat Silprasert <natsilprasert@gmail.com>
2025-10-22 11:17:20 +0700

    queue min

---
+++ queue_min/student.h
@@ -0,0 +1,23 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+template <typename Comp>
+T CP::queue<T>::min_element(std::vector<size_t> pos,Comp comp) const {
+  T tmp;
+  std::sort(pos.begin(), pos.end());
+  for (int i = 0; i < pos.size(); i++) {
+    if (i == 0) {
+      tmp = mData[(mFront + pos[i]) % mCap];
+    }
+    else if (pos[i] < mSize && comp(mData[(mFront + pos[i]) % mCap], tmp)) {
+      tmp = mData[(mFront + pos[i]) % mCap];
+    }
+  }
+  return tmp;
+}
+
+#endif
