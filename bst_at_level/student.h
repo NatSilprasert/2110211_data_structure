6d195c14f43f8ea3163b2d0736692525e43b0bde
Nat Silprasert <natsilprasert@gmail.com>
2025-11-17 15:20:00 +0700

    bst at level

---
+++ bst_at_level/student.h
@@ -0,0 +1,32 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+// you can modify and use this function
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+void CP::map_bst<KeyT, MappedT, CompareT>::my_recur(node *n, size_t level, size_t tmp, std::vector<KeyT> &v) {
+  if (n == nullptr) return;
+  if (tmp == level) {
+    v.push_back(n->data.first);
+    return;
+  }
+  tmp++;
+  my_recur(n->right, level, tmp, v);
+  my_recur(n->left, level, tmp, v);
+}
+
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+std::vector<KeyT> CP::map_bst<KeyT, MappedT, CompareT>::at_level(size_t level) {
+  std::vector<KeyT> ans;
+  my_recur(mRoot, level, 0, ans);
+  return ans;
+}
+
+#endif
