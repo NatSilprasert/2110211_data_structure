84d496cea6986ca68c61348b59ea58466857db26
Nat Silprasert <natsilprasert@gmail.com>
2025-11-17 13:11:14 +0700

    lcsr

--- lcrs/student.h
+++ lcrs/student.h
@@ -1,6 +1,13 @@
-// You may include library here
+int depth(node *n) {
+  if (n == nullptr) return -1;
+  if (n->left_child == nullptr) return 0;
+  
+  int result = depth(n->left_child);
 
-int depth(node *n)
-{
-  // Write Code Here
+  node *it = n->left_child;
+  while (it->right_sibling != nullptr) {
+    it = it->right_sibling;
+    result = std::max(result, depth(it));
+  }
+  return result + 1;
 }
